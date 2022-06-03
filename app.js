803910a435ae16949e74bcede49326677e50486e
Hannah <hj14569@gmail.com>
2022-06-02 22:13:36 -0700

    Connect to DB and add new items to DB.

--- app.js
+++ app.js
@@ -1,32 +1,56 @@
 //jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
-let items = ["10-minute Squat", "15-minute Meditation", "Read a book"];
-let workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemsSchema = {
+  name: String,
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+const item1 = new Item({
+  name: "Welcome to your to-do list!",
+});
+const item2 = new Item({
+  name: "Hit the + button to add a new item.",
+});
+const item3 = new Item({
+  name: "<-- Hit this to delete an item.",
+});
+const defaultItems = [item1, item2, item3];
+
 app.get("/", function (req, res) {
-  let day = date.getDate();
-  res.render("list", { listTitle: day, newListItems: items });
+  Item.find({}, function (err, results) {
+    if (results.length === 0) {
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully saved default items to DB");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", { listTitle: "Today", newListItems: results });
+    }
+  });
 });
 
 app.post("/", function (req, res) {
-  let item = req.body.newItem;
-
-  if (req.body.list == "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const itemName = req.body.newItem;
+  const item = new Item({
+    name: itemName,
+  });
+  item.save();
+  res.redirect("/");
 });
 
 app.get("/work", function (req, res) {
--- date.js
+++
@@ -1,9 +0,0 @@
-exports.getDate = function () {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-  return (day = today.toLocaleDateString("en-US", options));
-};
